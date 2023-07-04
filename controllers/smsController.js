bf2e4438ab4504d5a5c25f5a7dadbba2b45bb52f
Neo87z <shehanhoradagoda12@gmail.com>
2023-07-04 12:37:04 -0230

    first commit

--- controllers/smsController.js
+++ controllers/smsController.js
@@ -10,8 +10,9 @@ const newPrize = require('../models/Prize');
 const accountSID = 'AC5f7a45a9ed4f2b17a662df8339fcc3c7'
 const authtoken = process.env.TOKE_ID
 
-const httpDat= require('http');
-const MessagingResponse= require('twilio').twiml.MessagingResponse;
+const httpDat = require('http');
+const { MessagingResponse } = require('twilio').twiml;
+
 
 module.exports = function () {
     // POST route to add a new claim to the database
@@ -27,21 +28,22 @@ module.exports = function () {
             to: '+17093510576',
             body: "Oldtown"
         })
-       
+
 
 
 
     });
 
     router.post('/sms', (req, res) => {
-     const twiml= new MessagingResponse()
+        const twiml = new MessagingResponse();
 
-     twiml.messages('Welcome tO Oldtown SMS Order Service')
-     res.writeHead(200,{'Content-Type':'text/xml'});
-     res.end(twiml.toString());
-    
+        twiml.message('Welcome TO Oldtown SMS Order Portal 1- Large Pep Pizza, 2- Small Garlic ');
+
+        res.type('text/xml').send(twiml.toString());
+
+
+
     });
-
 
 
 
